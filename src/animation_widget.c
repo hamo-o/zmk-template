5792dcdcc96f8cec3ce15b7f9819ece9b8928d12
hamo-o <yheel369@gmail.com>
2026-05-11 21:33:44 +0900

    fix: zmk에서 미지원하는 기능 직접 구현

--- src/animation_widget.c
+++ src/animation_widget.c
@@ -4,6 +4,13 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
+static void animation_timer_cb(lv_timer_t *timer) {
+    struct animation_widget *widget = timer->user_data;
+
+    widget->current_frame = (widget->current_frame + 1) % ANIMATION_FRAME_COUNT;
+    lv_img_set_src(widget->img, animation_frames[widget->current_frame]);
+}
+
 int animation_widget_init(struct animation_widget *widget, lv_obj_t *parent) {
     widget->obj = lv_obj_create(parent);
     lv_obj_set_size(widget->obj, LV_PCT(100), LV_PCT(100));
@@ -11,15 +18,14 @@ int animation_widget_init(struct animation_widget *widget, lv_obj_t *parent) {
     lv_obj_set_style_border_width(widget->obj, 0, LV_PART_MAIN);
     lv_obj_set_style_bg_opa(widget->obj, LV_OPA_TRANSP, LV_PART_MAIN);
 
-    widget->anim_img = lv_animimg_create(widget->obj);
-    lv_obj_center(widget->anim_img);
+    widget->current_frame = 0;
+    widget->img = lv_img_create(widget->obj);
+    lv_obj_center(widget->img);
+    lv_img_set_src(widget->img, animation_frames[0]);
 
-    lv_animimg_set_src(widget->anim_img,
-                       (const void **)animation_frames,
-                       ANIMATION_FRAME_COUNT);
-    lv_animimg_set_duration(widget->anim_img, ANIMATION_DURATION_MS);
-    lv_animimg_set_repeat_count(widget->anim_img, LV_ANIM_REPEAT_INFINITE);
-    lv_animimg_start(widget->anim_img);
+    widget->timer = lv_timer_create(animation_timer_cb,
+                                     ANIMATION_DURATION_MS / ANIMATION_FRAME_COUNT,
+                                     widget);
 
     return 0;
 }
--- src/animation_widget.h
+++ src/animation_widget.h
@@ -6,7 +6,9 @@
 struct animation_widget {
     sys_snode_t node;
     lv_obj_t *obj;
-    lv_obj_t *anim_img;
+    lv_obj_t *img;
+    lv_timer_t *timer;
+    uint8_t current_frame;
 };
 
 int animation_widget_init(struct animation_widget *widget, lv_obj_t *parent);
